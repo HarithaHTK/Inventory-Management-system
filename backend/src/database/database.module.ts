cd64c84d25824444d21558eb592e95f50505d636
HarithaHTK <harithathushanka@gmail.com>
2025-12-23 18:28:39 +0530

    Connect data base and add test.

---
+++ backend/src/database/database.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: ['dist/**/*.entity.js'],
+        synchronize: configService.get('NODE_ENV') !== 'production',
+        logging: true,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
